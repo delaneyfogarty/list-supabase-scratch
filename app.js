c60f11021d5dfe5db954c393a02f14591dead3c8
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-22 17:56:42 -0700

    event listener for placement list

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 import { fetchAllPlacements } from './fetch-utils.js';
+import { renderAllPlacements } from './render-utils.js';
 
-window.addEventListener() {
+const placementListEl = document.querySelector('.placement-list');
+window.addEventListener('load', async () => {
+    fetchAndDisplayPlacements();
+});
 
+async function fetchAndDisplayPlacements() {
+
+    const placements = await fetchAllPlacements();
+
+    for (let placement of placements) {
+        const placementEl = renderAllPlacements(placement);
+
+        placementListEl.append(placementEl);
+    }
 }
